f7849cc1f5d39ba441a119982fef3d5313b8dc2c
QuocHoang94 <39055816+QuocHoang94@users.noreply.github.com>
2026-05-21 13:16:12 +0700

    code

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,10 @@
-export default defineConfig<TestOptions>({
 import { defineConfig } from '@playwright/test';
 import type { TestOptions } from './test_option';
-import type { TestOptions } from './test_option';
 
-require("dotenv").config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig<TestOptions>({
-  // testDir: './tests',
   testDir: './src/tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -21,123 +19,82 @@ export default defineConfig<TestOptions>({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    launchOptions:{
-      args:["--start-maximized"]
+    launchOptions: {
+      args: ['--start-maximized'],
     },
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: {
-      mode: 'on'
-    }
+      mode: 'on',
+    },
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name:'apiTests',
-      testMatch:'getRetrieveAllBreeds_api.spec.ts',
+      name: 'apiTests',
+      testMatch: 'getRetrieveAllBreeds_api.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'breedDetailTests',
-      testMatch:'getRetrieve_specific_breed.spec.ts',
+      name: 'breedDetailTests',
+      testMatch: 'getRetrieve_specific_breed.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'uploadImageTests',
-      testMatch:'uploadImage.spec.ts',
+      name: 'uploadImageTests',
+      testMatch: 'uploadImage.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'deleteImageTests',
-      testMatch:'deleteImageFromSpecificBreed.spec.ts',
+      name: 'deleteImageTests',
+      testMatch: 'deleteImageFromSpecificBreed.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'getUploadedImageTests',
-      testMatch:'getUploadedImageById.spec.ts',
+      name: 'getUploadedImageTests',
+      testMatch: 'getUploadedImageById.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'randomImagesTests',
-      testMatch:'getRandomImages.spec.ts',
+      name: 'randomImagesTests',
+      testMatch: 'getRandomImages.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'displayUploadedImageTests',
-      testMatch:'displayUploadedImage.spec.ts',
+      name: 'displayUploadedImageTests',
+      testMatch: 'displayUploadedImage.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
     },
     {
-      name:'uploadDelete204Tests',
-      testMatch:'uploadThenDeleteImage.spec.ts',
+      name: 'uploadDelete204Tests',
+      testMatch: 'uploadThenDeleteImage.spec.ts',
       use: {
         browserName: 'chromium',
         viewport: null,
       },
-    }
-    // {
-    //   name: 'chromium',
-    //   use: { ...devices['Desktop Chrome'] },
-    // },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
